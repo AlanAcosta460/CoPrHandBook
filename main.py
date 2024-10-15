956582cbc0ac048fffe189878af1be4967f9b688
Alan Acosta <alanomar460@gmail.com>
2024-10-15 11:24:26 -0600

    new format

--- main.py
+++ main.py
@@ -1,54 +1,81 @@
 import os
 
-def get_sections(directory):
-    section_names = {}
+def get_dirs(directory):
+    dir_names = {}
     for name in os.listdir(directory):
         if os.path.isdir(os.path.join(directory, name)):
             name = name.split('_', 1)
-            section_number = int(name[0])
-            section_name = name[1]
-            section_names[section_number] = section_name
-    return section_names
+            dir_number = int(name[0])
+            dir_name = name[1]
+            dir_names[dir_number] = dir_name
+    return dir_names
 
-def get_subsections(directory):
-    subsection_names = {}
+def get_files(directory):
+    file_names = {}
     for name in os.listdir(directory):
         name = name.split('_', 1)
-        subsection_number = int(name[0])
-        subsection_name = name[1]
-        subsection_names[subsection_number] = subsection_name
-    return subsection_names
+        file_number = int(name[0])
+        file_name = name[1]
+        file_names[file_number] = file_name
+    return file_names
+
+def append_tex(doc_file, title, description, text):
+    doc_file.write(f'\\subsubsection*{{{title}}}\n')
+    doc_file.write(f'{description}\n')
+    doc_file.write('\\begin{lstlisting}')
+    for line in text[1:]:
+        doc_file.write(f'{line}')
+    doc_file.write('\\end{lstlisting}\n\n')
 
 def main():
     doc_path = './Tex/handBook.tex'
     
     main_directory = './Algorithms/'
-    section_names = get_sections(main_directory)
+    sec_names = get_dirs(main_directory)
 
     with open(doc_path, 'w', encoding='utf-8') as doc_file:
-        for number, name in sorted(section_names.items()):
-            directory = main_directory + str(number) + '_' + name + '/'
+        for number, name in sorted(sec_names.items()):
+            sub_directory = main_directory + str(number) + '_' + name + '/'
 
             doc_file.write(f'\\section{{{name}}}\n')
 
-            subsection_names = get_subsections(directory)
+            subsec_names = get_dirs(sub_directory)
 
-            for number, name in sorted(subsection_names.items()):
-                file = directory + str(number) + '_' + name
-                
-                with open(file, 'r', encoding='utf-8') as f:
-                    text = f.readlines()
+            if not subsec_names:
+                subsec_names = get_files(sub_directory)
 
-                preamble = text[0].split('// ', 2)
-                title = preamble[1]
-                description = preamble[2]
+                for number, name in sorted(subsec_names.items()):
+                    file = sub_directory + str(number) + '_' + name
+                    
+                    with open(file, 'r', encoding='utf-8') as f:
+                        text = f.readlines()
+
+                    preamble = text[0].split('// ', 2)
+                    title = preamble[1]
+                    description = preamble[2]
+
+                    append_tex(doc_file, title, description, text)
+            else: 
+                for number, name in sorted(subsec_names.items()):
+                    subsub_directory = sub_directory + str(number) + '_' + name + '/'
+
+                    doc_file.write(f'\\subsection{{{name}}}\n')
+
+                    subsubsec_names = get_files(subsub_directory)
+
+                    for number, name in sorted(subsubsec_names.items()):
+                        file = subsub_directory + str(number) + '_' + name
+                        
+                        with open(file, 'r', encoding='utf-8') as f:
+                            text = f.readlines()
+
+                        preamble = text[0].split('// ', 2)
+                        title = preamble[1]
+                        description = preamble[2]
+
+                        append_tex(doc_file, title, description, text)
                 
-                doc_file.write(f'\\subsection{{{title}}}\n')
-                doc_file.write(f'{description}\n')
-                doc_file.write('\\begin{lstlisting}')
-                for line in text[1:]:
-                    doc_file.write(f'{line}')
-                doc_file.write('\\end{lstlisting}\n\n')
+                
             
         
 if __name__ == '__main__':
